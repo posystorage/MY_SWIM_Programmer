e2c2b8cbff672806014aeffc00bf53512e256abe
posystorage <qq2545889167@gmail.com>
2017-09-07 23:19:23 +0800

    usb枚举完成

---
+++ USB/CONFIG/usb_desc.c
@@ -0,0 +1,151 @@
+/**
+  ******************************************************************************
+  * @file    usb_desc.c
+  * @author  MCD Application Team
+  * @version V4.0.0
+  * @date    21-January-2013
+  * @brief   Descriptors for Virtual Com Port Demo
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "usb_lib.h"
+#include "usb_desc.h"
+
+/* USB Standard Device Descriptor */
+const uint8_t Virtual_Com_Port_DeviceDescriptor[] =
+  {
+    0x12,   /* bLength */
+    USB_DEVICE_DESCRIPTOR_TYPE,     /* bDescriptorType */
+    0x00,
+    0x02,   /* bcdUSB = 2.00 */
+    0x00,   /* bDeviceClass: CDC */
+    0x00,   /* bDeviceSubClass */
+    0x00,   /* bDeviceProtocol */
+    0x40,   /* bMaxPacketSize0 */
+    0x83,
+    0x04,   /* idVendor = 0x0483 */
+    0x48,
+    0x37,   /* idProduct = 0x3748 */
+    0x00,
+    0x01,   /* bcdDevice = 2.00 */
+    1,              /* Index of string descriptor describing manufacturer */
+    2,              /* Index of string descriptor describing product */
+    3,              /* Index of string descriptor describing the device's serial number */
+    0x01    /* bNumConfigurations */
+  };
+
+const uint8_t Virtual_Com_Port_ConfigDescriptor[] =
+  {
+    /*Configuration Descriptor*/
+    0x09,   /* bLength: Configuration Descriptor size */
+    USB_CONFIGURATION_DESCRIPTOR_TYPE,      /* bDescriptorType: Configuration */
+    0x27,       /* wTotalLength:no of returned bytes */
+    0x00,
+    0x01,   /* bNumInterfaces: 1 interface */
+    0x01,   /* bConfigurationValue: Configuration value */
+    0x00,   /* iConfiguration: Index of string descriptor describing the configuration */
+    0x80,   /* bmAttributes: self powered */
+    0x32,   /* MaxPower 32 mA */
+    /*Interface Descriptor*/
+    0x09,   /* bLength: Interface Descriptor size */
+    USB_INTERFACE_DESCRIPTOR_TYPE,  /* bDescriptorType: Interface */
+    /* Interface descriptor type */
+    0x00,   /* bInterfaceNumber: Number of Interface */
+    0x00,   /* bAlternateSetting: Alternate setting */
+    0x03,   /* bNumEndpoints: 3 endpoints used */
+    0xff,   /* bInterfaceClass: Communication Interface Class */
+    0xff,   /* bInterfaceSubClass: Abstract Control Model */
+    0xff,   /* bInterfaceProtocol: Common AT commands */
+    0x04,   /* iInterface: */
+
+		
+    /*Endpoint 1 Descriptor*/
+    0x07,   /* bLength: Endpoint Descriptor size */
+    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
+    0x81,   /* bEndpointAddress: (IN1) */
+    0x02,   /* bmAttributes: Bulk */
+    0x40,             /* wMaxPacketSize: */
+    0x00,
+    0x00,    /* bInterval */		
+    /*Endpoint 2 Descriptor*/
+    0x07,   /* bLength: Endpoint Descriptor size */
+    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
+    0x02,   /* bEndpointAddress: (OUT2) */
+    0x02,   /* bmAttributes: Bulk */
+    0x40,      /* wMaxPacketSize: */
+    0x00,
+    0x00,   /* bInterval:  ignore for Bulk transfer */
+    /*Endpoint 3 Descriptor*/
+    0x07,   /* bLength: Endpoint Descriptor size */
+    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
+    0x83,   /* bEndpointAddress: (IN3) */
+    0x02,   /* bmAttributes: Bulk */
+    0x40,             /* wMaxPacketSize: */
+    0x00,
+    0x00   /* bInterval: ignore for Bulk transfer */
+  };
+
+/* USB String Descriptors */
+const uint8_t Virtual_Com_Port_StringLangID[VIRTUAL_COM_PORT_SIZ_STRING_LANGID] =
+  {
+    VIRTUAL_COM_PORT_SIZ_STRING_LANGID,
+    USB_STRING_DESCRIPTOR_TYPE,
+    0x09,
+    0x04 /* LangID = 0x0409: U.S. English */
+  };
+
+const uint8_t Virtual_Com_Port_StringVendor[VIRTUAL_COM_PORT_SIZ_STRING_VENDOR] =
+  {
+    VIRTUAL_COM_PORT_SIZ_STRING_VENDOR,     /* Size of Vendor string */
+    USB_STRING_DESCRIPTOR_TYPE,             /* bDescriptorType*/
+    /* Manufacturer: "STMicroelectronics" */
+    'S', 0, 'T', 0, 'M', 0, 'i', 0, 'c', 0, 'r', 0, 'o', 0, 'e', 0,
+    'l', 0, 'e', 0, 'c', 0, 't', 0, 'r', 0, 'o', 0, 'n', 0, 'i', 0,
+    'c', 0, 's', 0
+  };
+
+const uint8_t Virtual_Com_Port_StringProduct[VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT] =
+  {
+    VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT,          /* bLength */
+    USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
+    /* Product name: "STM32 STLink" */
+    'S', 0, 'T', 0, 'M', 0, '3', 0, '2', 0, ' ', 0, 'S', 0, 'T', 0,
+    'L', 0, 'i', 0, 'n', 0, 'k', 0
+  };
+
+uint8_t Virtual_Com_Port_StringSerial[VIRTUAL_COM_PORT_SIZ_STRING_SERIAL] =
+  {
+    VIRTUAL_COM_PORT_SIZ_STRING_SERIAL,           /* bLength */
+    USB_STRING_DESCRIPTOR_TYPE,                   /* bDescriptorType */
+    'S', 0, 'T', 0, 'M', 0, '3', 0, '2', 0
+  };
+	
+	
+const uint8_t STlink_String_debug[STlink_String_debug_num] =
+  {
+    STlink_String_debug_num,           /* bLength */
+    USB_STRING_DESCRIPTOR_TYPE,                   /* bDescriptorType */
+    'S', 0, 'T', 0, '-', 0, 'L', 0, 'i', 0, 'n', 0, 'k', 0, ' ', 0,
+    'D', 0, 'e', 0, 'b', 0, 'u', 0, 'g', 0
+  };	
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- USER/main.c
+++ USER/main.c
@@ -4,6 +4,12 @@
 #include "timer.h"
 #include "usart.h"
 #include "swim.h"
+
+#include "usb_lib.h"
+#include "hw_config.h"
+#include "usb_pwr.h"	
+
+
 #include "SEGGER_SYSVIEW.h"
 
 
@@ -14,7 +20,17 @@
 	uart_init(115200);
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
 	//LED_Init();		  	//初始化与LED连接的硬件接口
-	//TIM3_Int_Init(4999,7199);//10Khz的计数频率，计数到5000为500ms  
+	//TIM3_Int_Init(4999,7199);//10Khz的计数频率，计数到5000为500ms 
+
+	delay_ms(500);
+	USB_Port_Set(0); 	//USB先断开
+	delay_ms(100);
+	USB_Port_Set(1);	//USB再次连接
+ 	Set_USBClock();   
+ 	USB_Interrupts_Config();    
+ 	USB_Init();	
+
+	 
 	 SWIM_Init();
 	 SWIM_EnterProgMode();
 	 SWIM_CUT_OFF();
